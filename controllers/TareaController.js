cfac2d40e26830ef8bf61c1f8bc76da31f932f98
Jaime A <jaimeangel1097@gmail.com>
2023-07-19 17:17:24 -0500

    creando controllers restantes modelo Tareas

--- controllers/TareaController.js
+++ controllers/TareaController.js
@@ -1,15 +1,108 @@
-const crearTarea=(req,res)=>{
-    
+import Tarea from '../models/Tarea.js'
+import Proyecto from '../models/Proyecto.js'
+
+const crearTarea= async (req,res)=>{
+    const tarea=req.body;
+    const {user}=req;
+
+    try {
+        const proyectExist= await Proyecto.findById(tarea.proyecto)
+
+        if(proyectExist.creador.toString() !== user._id.toString()){
+            const errorMsg= new Error('No tienes los permisos para realizar esta accion al proyecto')
+            return res.status(401).json({msg:errorMsg.message})
+        }
+
+        const newTarea=new Tarea(tarea)
+
+        try {
+            await newTarea.save()
+            return res.json(newTarea)
+        } catch (error) {
+            const errorMsg= new Error('Algo salio mal')
+            console.log(error)
+            return res.status(403).json({msg:errorMsg.message})
+        }
+    } catch (error) {
+        const errorMsg= new Error('El proyecto no existe')
+        return res.status(404).json({msg:errorMsg.message})
+    }
 }
-const editarTarea=(req,res)=>{
-    
+const editarTarea= async (req,res)=>{
+    const {tarea}=req.params;
+    const {user}=req;
+
+    try {
+        const tareaExist = await Tarea.findById(tarea).populate("proyecto")
+
+        if(tareaExist.proyecto.creador.toString() !== user._id.toString()){
+            const errorMsg= new Error('No tienes los permisos para realizar esta accion al proyecto')
+            return res.status(401).json({msg:errorMsg.message})
+        }
+
+        const valueToUpdate= Object.keys(req.body)
+        valueToUpdate.forEach(value =>{
+            if(value in tareaExist){
+                tareaExist[value]=req.body[value]
+            }
+        })
+        
+        try {
+            await tareaExist.save()
+            res.json({msg:"Se guardaron los cambios con exito"})
+        } catch (error) {
+            console.log(error)
+        }
+
+    } catch (error) {
+        const errorMsg= new Error('algo salio mal')
+        return res.status(404).json({msg:errorMsg.message})
+    }
 }
-const eliminarTarea=(req,res)=>{
-    
+const eliminarTarea= async (req,res)=>{
+    const {tarea}=req.params;
+    const {user}=req;
+
+    try {
+        const tareaExist = await Tarea.findById(tarea).populate("proyecto")
+
+        if(tareaExist.proyecto.creador.toString() !== user._id.toString()){
+            const errorMsg= new Error('No tienes los permisos para realizar esta accion al proyecto')
+            return res.status(401).json({msg:errorMsg.message})
+        }
+
+        try {
+            await tareaExist.deleteOne()
+            return res.json({msg:'La tarea fue eliminidad con exito'})
+        } catch (error) {
+            const errorMsg= new Error('Algo salio mal')
+            console.log(error)
+            return res.status(403).json({msg:errorMsg.message})
+        }
+    } catch (error) {
+        const errorMsg= new Error('Algo salio mal')
+        return res.status(404).json({msg:errorMsg.message})
+    }
 }
-const obtenerTarea=(req,res)=>{
-    
+const obtenerTarea= async (req,res)=>{
+    const {tarea}=req.params;
+    const {user}=req;
+
+    try {
+        const tareaExist = await Tarea.findById(tarea).populate("proyecto")
+
+        if(tareaExist.proyecto.creador.toString() !== user._id.toString()){
+            const errorMsg= new Error('No tienes los permisos para realizar esta accion al proyecto')
+            return res.status(401).json({msg:errorMsg.message})
+        }
+
+        return res.json(tareaExist)
+    }catch(error) {
+        const errorMsg = new Error('algo salio mal')
+        return res.status(404).json({msg:errorMsg.message})
+    }
 }
+
 const cambiarEstadoTarea=(req,res)=>{
     
 }
--- routes/TareaRoutes.js
+++ routes/TareaRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-export {
+import {
     crearTarea,
     editarTarea,
     eliminarTarea,
